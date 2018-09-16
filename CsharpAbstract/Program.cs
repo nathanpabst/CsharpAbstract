81a5da87f94c7c66c4255850832a729f9148fa32
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-16 10:33:40 -0500

    Add project files.

---
+++ CsharpAbstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpAbstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
